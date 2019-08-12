5c88f2fdb94d51f984b8b6918ac90e17a7d2f384
Luís Augusto Weber Mercado <luiswebmercado@gmail.com>
2019-08-12 17:52:28 +0200

    Adding an installation script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+composer install
+mkdir storage/uploaded
+mkdir storage/framework
+mkdir storage/framework/sessions
+mkdir storage/framework/cache
+mkdir storage/framework/views
+
+cd public && bower install
